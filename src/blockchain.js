dec9e8ed0f12f2d9f8195f01d4fef0f101d94424
keicila <keicilamarcial2@gmail.com>
2021-06-09 23:50:57 -0300

    feat
    class: blockchain => Adiciondas as funções getLastBlock e  addBlock

--- src/blockchain.js
+++ src/blockchain.js
@@ -8,10 +8,16 @@ const Block = require ('../src/block')
     }
 
     getLastBlock(){
-      
+      return  this.blocks[this.blocks.length-1]
     }
     addBlock(serializedObject){
-      
+       const index = this.index
+       const previousHash = this.getLastBlock().hash
+       const difficulties = this.difficulties
+       const block = new Block(index,previousHash,serializedObject,difficulties)
+       this.index++
+       this.blocks.push(block)
+       console.log(this.blocks)
     }
     isValid(){
         //TDD
